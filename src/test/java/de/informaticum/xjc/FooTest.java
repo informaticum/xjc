9fa02e8a22ec5e0716cd3bf4e59d006424b9f247
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2021-03-29 14:23:02 +0200

    assertThat(x).hasToString(s) over assertThat(x.toString()).isEqualTo(s)

--- src/test/java/de/informaticum/xjc/FooTest.java
+++ src/test/java/de/informaticum/xjc/FooTest.java
@@ -11,8 +11,7 @@ public class FooTest {
     throws Exception {
         final var plugin = new BarPlugin();
         // System.out.println(plugin.getUsage());
-        assertThat(plugin.getUsage()
-                   .toString()).isEqualTo(format("  -bar :  Some bar infomation.%n  -aaaaaa :  Some aaaaaa information. Default: aaaaaa%n  -bb     :  Some bb information. Default: bb%n"));
+        assertThat(plugin.getUsage()).hasToString(format("  -bar :  Some bar infomation.%n  -aaaaaa :  Some aaaaaa information. Default: aaaaaa%n  -bb     :  Some bb information. Default: bb%n"));
     }
 
 }
