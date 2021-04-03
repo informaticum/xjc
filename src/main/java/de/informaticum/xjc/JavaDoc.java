b54fabc0a156f1701855f7bd32c5994adaef9893
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2021-04-04 00:34:43 +0200

    PARAM_* lexikografisch sortieren

--- src/main/java/de/informaticum/xjc/JavaDoc.java
+++ src/main/java/de/informaticum/xjc/JavaDoc.java
@@ -9,15 +9,15 @@ public enum JavaDoc {
 
     public static final String DEFAULT_FIELD_ASSIGNMENT = "%n%nThe field {@link #%s} will be initialised with: {@code %s}";
 
-    public static final String PARAM_WITH_DEFAULT_MULTI_VALUE = "value for the attribute '%s' (can be {@code null} because an empty, modifiable list will be used instead)";
-
-    public static final String PARAM_WITH_DEFAULT_SINGLE_VALUE = "value for the attribute '%s' (can be {@code null} because an according default value will be used instead)";
-
     public static final String PARAM_THAT_IS_OPTIONAL = "value for the attribute '%s' (can be {@code null} because attribute is optional)";
 
     public static final String PARAM_THAT_IS_PRIMITIVE = "value for the attribute '%s'";
 
     public static final String PARAM_THAT_IS_REQUIRED = "value for the attribute '%s' (cannot be {@code null} because attribute is required)";
+
+    public static final String PARAM_WITH_DEFAULT_MULTI_VALUE = "value for the attribute '%s' (can be {@code null} because an empty, modifiable list will be used instead)";
+
+    public static final String PARAM_WITH_DEFAULT_SINGLE_VALUE = "value for the attribute '%s' (can be {@code null} because an according default value will be used instead)";
 
     public static final String RETURN_OPTIONAL_VALUE = "the value of the optional attribute '%s'";
 
