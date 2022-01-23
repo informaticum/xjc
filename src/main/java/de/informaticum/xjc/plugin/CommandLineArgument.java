3bb377b1c38f7ce5877f3022e6cfff6a157af9d9
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2022-01-23 16:47:51 +0100

    separate xjc-option behaviour from commandline-option

--- src/main/java/de/informaticum/xjc/plugin/CommandLineArgument.java
+++ src/main/java/de/informaticum/xjc/plugin/CommandLineArgument.java
@@ -1,21 +1,12 @@
 package de.informaticum.xjc.plugin;
 
-import static de.informaticum.xjc.util.OutlineAnalysis.fullNameOf;
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.slf4j.LoggerFactory.getLogger;
-import java.util.function.Consumer;
-import com.sun.codemodel.JDefinedClass;
 import com.sun.tools.xjc.Options;
-import com.sun.tools.xjc.outline.ClassOutline;
-import com.sun.tools.xjc.outline.EnumOutline;
-import com.sun.tools.xjc.outline.PackageOutline;
-import org.slf4j.Logger;
 
-public class CommandLineArgument {
-
-    private static final Logger LOG = getLogger(CommandLineArgument.class);
+public final class CommandLineArgument
+implements XjcOption {
 
     private final String argument;
 
@@ -23,16 +14,13 @@ public class CommandLineArgument {
 
     private boolean activated;
 
-    public CommandLineArgument(final String argument) {
-        this(argument, "%TBD description%");
-    }
-
     public CommandLineArgument(final String argument, final String description) {
         this.argument = requireNonNull(argument).startsWith("-") ? argument : "-" + argument;
         this.description = requireNonNull(description);
         this.activated = false;
     }
 
+    @Override
     public final String getArgument() {
         return this.argument;
     }
@@ -41,6 +29,7 @@ public class CommandLineArgument {
         return this.description;
     }
 
+    @Override
     public final boolean isActivated() {
         return this.activated;
     }
@@ -54,46 +43,6 @@ public class CommandLineArgument {
     public final void alsoActivate(final CommandLineArgument... activateImplicitly) {
         if (this.activated) {
             asList(activateImplicitly).forEach(arg -> arg.activated = true);
-        }
-    }
-
-    public final void doOnActivation(final Runnable execution) {
-        if (this.activated) {
-            execution.run();
-        } else {
-            LOG.trace("Skip execution of XJC option [{}], because it has not been activated.", this.argument);
-        }
-    }
-
-    public final void doOnActivation(final Consumer<? super PackageOutline> execution, final PackageOutline pakkage) {
-        if (this.activated) {
-            execution.accept(pakkage);
-        } else {
-            LOG.trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.argument, fullNameOf(pakkage));
-        }
-    }
-
-    public final void doOnActivation(final Consumer<? super JDefinedClass> execution, final JDefinedClass $factory) {
-        if (this.activated) {
-            execution.accept($factory);
-        } else {
-            LOG.trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.argument, $factory.fullName());
-        }
-    }
-
-    public final void doOnActivation(final Consumer<? super ClassOutline> execution, final ClassOutline clazz) {
-        if (this.activated) {
-            execution.accept(clazz);
-        } else {
-            LOG.trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.argument, fullNameOf(clazz));
-        }
-    }
-
-    public final void doOnActivation(final Consumer<? super EnumOutline> execution, final EnumOutline enumeration) {
-        if (this.activated) {
-            execution.accept(enumeration);
-        } else {
-            LOG.trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.argument, fullNameOf(enumeration));
         }
     }
 
---
+++ src/main/java/de/informaticum/xjc/plugin/XjcOption.java
@@ -0,0 +1,71 @@
+package de.informaticum.xjc.plugin;
+
+import static de.informaticum.xjc.util.OutlineAnalysis.fullNameOf;
+import static org.slf4j.LoggerFactory.getLogger;
+import java.util.function.Consumer;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.EnumOutline;
+import com.sun.tools.xjc.outline.PackageOutline;
+
+public abstract interface XjcOption {
+
+    public abstract String getArgument();
+
+    public abstract boolean isActivated();
+
+    public default XjcOption or(final XjcOption other) {
+        return new XjcOption() {
+            @Override
+            public final String getArgument() {
+                return XjcOption.this.getArgument() + "/" + other.getArgument();
+            }
+
+            @Override
+            public final boolean isActivated() {
+                return XjcOption.this.isActivated() || other.isActivated();
+            }
+        };
+    }
+
+    public default void doOnActivation(final Runnable execution) {
+        if (this.isActivated()) {
+            execution.run();
+        } else {
+            getLogger(XjcOption.class).trace("Skip execution of XJC option [{}], because it has not been activated.", this.getArgument());
+        }
+    }
+
+    public default void doOnActivation(final Consumer<? super PackageOutline> execution, final PackageOutline pakkage) {
+        if (this.isActivated()) {
+            execution.accept(pakkage);
+        } else {
+            getLogger(XjcOption.class).trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.getArgument(), fullNameOf(pakkage));
+        }
+    }
+
+    public default void doOnActivation(final Consumer<? super JDefinedClass> execution, final JDefinedClass $factory) {
+        if (this.isActivated()) {
+            execution.accept($factory);
+        } else {
+            getLogger(XjcOption.class).trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.getArgument(), $factory.fullName());
+        }
+    }
+
+    public default void doOnActivation(final Consumer<? super ClassOutline> execution, final ClassOutline clazz) {
+        if (this.isActivated()) {
+            execution.accept(clazz);
+        } else {
+            getLogger(XjcOption.class).trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.getArgument(), fullNameOf(clazz));
+        }
+    }
+
+    public default void doOnActivation(final Consumer<? super EnumOutline> execution, final EnumOutline enumeration) {
+        if (this.isActivated()) {
+            execution.accept(enumeration);
+        } else {
+            getLogger(XjcOption.class).trace("Skip execution of XJC option [{}] for [{}], because it has not been activated.", this.getArgument(), fullNameOf(enumeration));
+        }
+    }
+
+}
