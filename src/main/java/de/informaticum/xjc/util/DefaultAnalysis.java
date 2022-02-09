b5125c23776050cdb2624e652f9594856a152dcc
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2022-02-09 19:20:15 +0100

    consolidate #defaultValueFor(...) methods

--- src/main/java/de/informaticum/xjc/util/DefaultAnalysis.java
+++ src/main/java/de/informaticum/xjc/util/DefaultAnalysis.java
@@ -1,8 +1,8 @@
 package de.informaticum.xjc.util;
 
 import static com.sun.codemodel.JExpr.lit;
-import static de.informaticum.xjc.util.CollectionAnalysis.emptyModifiableInstanceOf;
 import static de.informaticum.xjc.util.CollectionAnalysis.emptyImmutableInstanceOf;
+import static de.informaticum.xjc.util.CollectionAnalysis.emptyModifiableInstanceOf;
 import static org.slf4j.LoggerFactory.getLogger;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
@@ -38,9 +38,8 @@ public enum DefaultAnalysis {
      * <dt>for any primitive type</dt>
      * <dd><a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html">the according Java default value</a> is chosen,</dd>
      * <dt>for any known collection type</dt>
-     * <dd>either {@code null} or {@linkplain CollectionAnalysis#emptyModifiableInstanceOf(com.sun.codemodel.JType) the according modifiable/unmodifiable empty instance} is
-     * chosen,</dd>
-     * <dd></dd>
+     * <dd>{@linkplain CollectionAnalysis#emptyModifiableInstanceOf(com.sun.codemodel.JType) the according modifiable/unmodifiable empty instance} may be chosen (if
+     * requested),</dd>
      * <dt>in any other cases</dt>
      * <dd>the {@linkplain Optional#empty() empty Optional} is returned.</dd>
      * </dl>
@@ -48,44 +47,13 @@ public enum DefaultAnalysis {
      * @param field
      *            the field to analyse
      * @param initCollections
-     *            either not {@linkplain CommandLineArgument#isActivated() activated} to initialise collection types with {@code null} or
-     *            {@linkplainCommandLineArgument#isActivated() activated} to initialise with the according modifiable/unmodifiable empty instance
+     *            in order to initialise collections with the according modifiable/unmodifiable empty instance supply {@code true}; otherwise supply {@code false} to do not
      * @param unmodifiable
-     *            either not {@linkplain CommandLineArgument#isActivated() activated} to create a modifiable empty collection instance or
-     *            {@linkplain CommandLineArgument#isActivated() activated} to create an unmodifiable empty collection instance
+     *            in case an empty collection instance is initialised, that instance may be either modifiable (supply {@code false}) or may be unmodifiable (supply {@code true})
      * @return an {@link Optional} holding the default value for the given field if such value exists; the {@linkplain Optional#empty() empty Optional} otherwise
      * @see <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html">The Java™ Tutorials :: Primitive Data Types</a>
      */
     public static final Optional<JExpression> defaultValueFor(final FieldOutline field, final BooleanSupplier initCollections, final BooleanSupplier unmodifiable) {
-        return defaultValueFor(field, initCollections.getAsBoolean(), unmodifiable.getAsBoolean());
-    }
-
-    /**
-     * Returns the the default value for the given field if such value exists. In detail, this means (in order):
-     * <dl>
-     * <dt>for any XSD attribute with a given lexical value</dt>
-     * <dd>{@linkplain com.sun.tools.xjc.model.CDefaultValue#compute(com.sun.tools.xjc.outline.Outline) the according Java expression} is chosen if it can be computed,</dd>
-     * <dt>for any primitive type</dt>
-     * <dd><a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html">the according Java default value</a> is chosen,</dd>
-     * <dt>for any known collection type</dt>
-     * <dd>either {@code null} or {@linkplain CollectionAnalysis#emptyModifiableInstanceOf(com.sun.codemodel.JType) the according modifiable/unmodifiable empty instance} is
-     * chosen,</dd>
-     * <dd>and further, the default instance may be modifiable or unmodifiable</dd>
-     * <dt>in any other cases</dt>
-     * <dd>the {@linkplain Optional#empty() empty Optional} is returned.</dd>
-     * </dl>
-     *
-     * @param field
-     *            the field to analyse
-     * @param initCollections
-     *            either {@code false} to initialise collection types with {@code null} or {@code true} to initialise with the according modifiable/unmodifiable empty instance
-     * @param unmodifiable
-     *            in case an empty collection instance is initialised, that instance may be either modifiable (pass {@code false} value) or may be unmodifiable (pass {@code true}
-     *            value)
-     * @return an {@link Optional} holding the default value for the given field if such value exists; the {@linkplain Optional#empty() empty Optional} otherwise
-     * @see <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html">The Java™ Tutorials :: Primitive Data Types</a>
-     */
-    public static final Optional<JExpression> defaultValueFor(final FieldOutline field, final boolean initCollections, final boolean unmodifiable) {
         final var outline = field.parent().parent();
         final var property = field.getPropertyInfo();
         if (property.defaultValue != null) {
@@ -99,17 +67,19 @@ public enum DefaultAnalysis {
         //       dass ein Default-Wert existiert. Das darf nicht passieren. Ein "Boolean" ist initial "null".
         // TODO: Consider property.isUnboxable()? What to do in that case?
         // TODO: Consider property.isOptionalPrimitive()? What to do in that case?
-        if (raw.equals(codeModel.BOOLEAN)) return Optional.of(lit(DEFAULT_BOOLEAN));
-        if (raw.equals(codeModel.BYTE   )) return Optional.of(lit(DEFAULT_BYTE   ));
-        if (raw.equals(codeModel.CHAR   )) return Optional.of(lit(DEFAULT_CHAR   ));
-        if (raw.equals(codeModel.DOUBLE )) return Optional.of(lit(DEFAULT_DOUBLE ));
-        if (raw.equals(codeModel.FLOAT  )) return Optional.of(lit(DEFAULT_FLOAT  ));
-        if (raw.equals(codeModel.INT    )) return Optional.of(lit(DEFAULT_INT    ));
-        if (raw.equals(codeModel.LONG   )) return Optional.of(lit(DEFAULT_LONG   ));
-        if (raw.equals(codeModel.SHORT  )) return Optional.of(lit(DEFAULT_SHORT  ));
-        if (property.isCollection() && initCollections &&  unmodifiable) return Optional.of(emptyImmutableInstanceOf( field.getRawType()));
-        if (property.isCollection() && initCollections && !unmodifiable) return Optional.of(emptyModifiableInstanceOf(field.getRawType()));
-        return Optional.empty();
+        if      (raw.equals(codeModel.BOOLEAN)) { return Optional.of(lit(DEFAULT_BOOLEAN)); }
+        else if (raw.equals(codeModel.BYTE   )) { return Optional.of(lit(DEFAULT_BYTE   )); }
+        else if (raw.equals(codeModel.CHAR   )) { return Optional.of(lit(DEFAULT_CHAR   )); }
+        else if (raw.equals(codeModel.DOUBLE )) { return Optional.of(lit(DEFAULT_DOUBLE )); }
+        else if (raw.equals(codeModel.FLOAT  )) { return Optional.of(lit(DEFAULT_FLOAT  )); }
+        else if (raw.equals(codeModel.INT    )) { return Optional.of(lit(DEFAULT_INT    )); }
+        else if (raw.equals(codeModel.LONG   )) { return Optional.of(lit(DEFAULT_LONG   )); }
+        else if (raw.equals(codeModel.SHORT  )) { return Optional.of(lit(DEFAULT_SHORT  )); }
+        else if (property.isCollection() && initCollections.getAsBoolean()) {
+            return Optional.of(unmodifiable.getAsBoolean() ? emptyImmutableInstanceOf(field.getRawType()) : emptyModifiableInstanceOf(field.getRawType()));
+        } else {
+            return Optional.empty();
+        }
     }
 
 }
