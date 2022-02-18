2ca0ec40684fecf03b83917dad31eab600507b35
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2022-02-18 20:32:02 +0100

    rename codemodel instances with "$" prefix

--- src/main/java/de/informaticum/xjc/util/CodeModelAnalysis.java
+++ src/main/java/de/informaticum/xjc/util/CodeModelAnalysis.java
@@ -8,23 +8,23 @@ import com.sun.codemodel.JType;
 public enum CodeModelAnalysis {
     ;
 
-    public static final JMethod getConstructor(final JDefinedClass $clazz, final JType... argumentTypes) {
-        final var constructor = $clazz.getConstructor(argumentTypes);
-        if (constructor != null) {
-            return constructor;
+    public static final JMethod getConstructor(final JDefinedClass $clazz, final JType... $argumentTypes) {
+        final var $constructor = $clazz.getConstructor($argumentTypes);
+        if ($constructor != null) {
+            return $constructor;
         } else {
-            final var rawTypes = stream(argumentTypes).map(JType::erasure).toArray(JType[]::new);
-            return $clazz.getConstructor(rawTypes);
+            final var $rawTypes = stream($argumentTypes).map(JType::erasure).toArray(JType[]::new);
+            return $clazz.getConstructor($rawTypes);
         }
     }
 
-    public static final JMethod getMethod(final JDefinedClass $clazz, final String name, final JType... argumentTypes) {
-        final var method = $clazz.getMethod(name, argumentTypes);
-        if (method != null) {
-            return method;
+    public static final JMethod getMethod(final JDefinedClass $clazz, final String name, final JType... $argumentTypes) {
+        final var $method = $clazz.getMethod(name, $argumentTypes);
+        if ($method != null) {
+            return $method;
         } else {
-            final var rawTypes = stream(argumentTypes).map(JType::erasure).toArray(JType[]::new);
-            return $clazz.getMethod(name, rawTypes);
+            final var $rawTypes = stream($argumentTypes).map(JType::erasure).toArray(JType[]::new);
+            return $clazz.getMethod(name, $rawTypes);
         }
     }
 
