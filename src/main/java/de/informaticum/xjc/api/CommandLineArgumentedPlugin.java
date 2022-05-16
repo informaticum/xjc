26198cc9aaef5f350a0105279ffdf21334cebc2f
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2022-05-16 22:42:48 +0200

    Brand-New: cadger arguments

--- src/main/java/de/informaticum/xjc/api/CommandLineArgumentedPlugin.java
+++ src/main/java/de/informaticum/xjc/api/CommandLineArgumentedPlugin.java
@@ -3,8 +3,10 @@ package de.informaticum.xjc.api;
 import static java.lang.String.format;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicBoolean;
 import com.sun.tools.xjc.BadCommandLineException;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.Plugin;
@@ -14,6 +16,12 @@ import com.sun.tools.xjc.Plugin;
  */
 public abstract class CommandLineArgumentedPlugin
 extends Plugin {
+
+    /**
+     * Marker boolean whether or not the command line's arguments have {@linkplain #parseArgument(Options, String[], int) yet been analysed} to activate
+     * {@linkplain #getCadgerArguments() the cadger arguments}.
+     */
+    private final AtomicBoolean cadged = new AtomicBoolean(false);
 
     /**
      * Gets the option name to use for XJC add-on activation; Plus, gets the add-on's description. The strong coupling of both attributes becomes manifest in the usage of the
@@ -56,6 +64,16 @@ extends Plugin {
         return emptyList();
     }
 
+    /**
+     * Is there any cadger (a.k.a. stealth-mode) plug-in argument? Just return all of them here and you will get {@linkplain #parseArgument(Options, String[], int) a default
+     * argument parsing} for free.
+     *
+     * @return all plug-in's command line arguments
+     */
+    public List<XjcOption> getCadgerArguments() {
+        return emptyList();
+    }
+
     @Override
     public final String getUsage() {
         final var pluginArgs = this.getPluginArguments();
@@ -66,6 +84,15 @@ extends Plugin {
             final var argNames = arg.getParameters().stream().map(s -> "<" + s + ">").collect(joining(" "));
             usage.append(format("  %1$-" + width + "s :  %2$s%n", arg.getArgument() + (argNames.isBlank() ? "" : " " + argNames), arg.getDescription()));
         });
+        if (!this.getCadgerArguments().isEmpty()) {
+            usage.append("  NOTE, this plug-in additionaly respects the intention of these options: ");
+            usage.append(this.getCadgerArguments().stream().map(arg -> {
+                final var argNames = arg.getParameters().stream().map(s -> "<" + s + ">").collect(joining(" "));
+                return arg.getArgument() + (argNames.isBlank() ? "" : " " + argNames);
+            }).collect(joining(", ")));
+            usage.append(format("%n"));
+            this.getCadgerArguments();
+        }
         return usage.toString();
     }
 
@@ -76,7 +103,19 @@ extends Plugin {
     @Override
     public final int parseArgument(final Options options, final String[] arguments, final int index)
     throws BadCommandLineException {
-        // Consume the current argument if it's either equal to this plug-in's option name or if it matches any of this plug-in's arguments
+        // First of all, do the cadger arguments parsing
+        if (this.cadged.compareAndSet(false, true)) {
+            for (var i = 0; i < arguments.length; i++) {
+                for (final var cadger : this.getCadgerArguments()) {
+                    if (cadger.getArgument().equals(arguments[i])) {
+                        final var consumed = cadger.parseArgument(options, arguments, i);
+                        assertThat(consumed).isPositive();
+                        i += (consumed - 1);
+                    }
+                }
+            }
+        }
+        // Now, consume the current argument if it's either equal to this plug-in's option name or if it matches any of this plug-in's arguments
         if (this.getOptionName().equals(arguments[index])) {
             return 1;
         } else {
