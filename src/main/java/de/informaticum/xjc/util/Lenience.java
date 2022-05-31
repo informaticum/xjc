c642028377e65ffafcc26f8b23cf4cda00a388e3
Stefan Gasterstädt <stefan.gasterstaedt@mailbox.org>
2022-05-31 23:22:15 +0200

    handle missing system property

--- src/main/java/de/informaticum/xjc/util/Lenience.java
+++ src/main/java/de/informaticum/xjc/util/Lenience.java
@@ -2,13 +2,14 @@ package de.informaticum.xjc.util;
 
 import static de.informaticum.xjc.util.i18n.LenienceMessages.SERIOUS_PROBLEM;
 import static java.lang.Boolean.parseBoolean;
+import static java.util.Optional.ofNullable;
 
 public enum Lenience {
     ;
 
     private static final String LENIENCE_PROPERTY = "informaticum.xjc.lenience";
 
-    public static final boolean LENIENT = parseBoolean(System.getProperty(LENIENCE_PROPERTY).strip());
+    public static final boolean LENIENT = parseBoolean(ofNullable(System.getProperty(LENIENCE_PROPERTY)).orElse("").strip());
 
     public static final String LENIENT_BREAKPOINT_MESSAGE = SERIOUS_PROBLEM.format(LENIENCE_PROPERTY);
 
